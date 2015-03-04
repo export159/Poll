8094db83155a360989c0d71fafd9ff2b2928cc81
Kirby <kherbz07@gmail.com>
2015-03-04 14:49:34 +0800

    Created mockup

---
+++ views/login/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Poll System - Team Bangan</title>
+</head>
+<body>
+	<div>
+		<form action="" method="POST">
+			<input type="text" id="login_username" name="username" />
+			<input type="password" id="login_password" name="password" />
+			<button type="submit">Login</button>
+		</form>
+	</div>
+	<hr/>
+	<div>
+		<form action="" method="POST">
+			<input type="text" id="full_name" name="full_name" />
+			<input type="text" id="username" name="username" />
+			<input type="password" id="password" name="password" />
+			<button type="submit">Sign Up</button>
+ 		</form>
+	</div>
+</body>
+</html>
