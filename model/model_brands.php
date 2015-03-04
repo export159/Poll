9497216f75212624465744fbfbe024d3d8840b99
Jerome Noveda <noveda.jerome@gmail.com>
2015-03-04 15:25:12 +0800

    added model_brands.php for brands related functions

---
+++ model/model_brands.php
@@ -0,0 +1,32 @@
+<?php 
+require_once "../includes/PDOConnector.php";
+
+class Model_brands extends PDOConnector{
+	
+	/*
+		get all brands in tbl_brands
+		
+		return: $result (dataType: array)
+	*/
+	public function getAllBrands(){
+		$this->connect();
+		$result = null;
+		$counter = 0;
+		try{
+			$sql = "SELECT * FROM tbl_brands";
+			$stmt = $this->dbh->prepare($sql);
+			$stmt->execute();
+			while($rs = $stmt->fetch()){
+				$result[$counter]['id'] = $rs['id'];
+				$result[$counter]['name'] = $rs['name'];
+				$counter++;
+			}
+			
+		}catch(PDOException $e){
+			print_r($e);
+		}
+		
+		$this->close();
+		return $result;
+	}
+}
